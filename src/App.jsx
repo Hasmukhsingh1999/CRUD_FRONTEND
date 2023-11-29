c67e59d5bfa5030c94d672eeef7ce3b569c8dcf7
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-29 09:20:04 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from "./pages/Books";
+import Add from "./pages/Add";
+import Update from "./pages/Update";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element/></Routes></BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update" element={<Update/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

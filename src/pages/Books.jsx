889d92c1d2933c198d77f4984cdd72893d2e4459
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-29 09:46:34 +0530

    added

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -3,19 +3,49 @@ import axios from "axios";
 
 const Books = () => {
   const [books, setBooks] = useState([]);
+
   useEffect(() => {
     const fetchAllBooks = async () => {
       try {
         const res = await axios.get("http://localhost:8800/books");
         console.log(res.data);
+        setBooks(res.data);
       } catch (error) {
         console.log(error);
       }
     };
-    fetchAllBooks(2);
+
+    fetchAllBooks();
   }, []);
 
-  return <div>Books</div>;
+  return (
+    <div>
+      <h1>Book Shop</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Cover</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <tr className="book" key={book.id}>
+              <td>{book.id}</td>
+              <td>{book.cover && <img src={book.cover} alt="" />}</td>
+              <td>{book.title}</td>
+              <td>{book.desc}</td>
+              <td>{book.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Books;
+

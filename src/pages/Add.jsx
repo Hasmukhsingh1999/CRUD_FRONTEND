893c084ea93cacca5e7baf6a9501241397b04e49
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-29 10:04:05 +0530

    addede

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,16 +1,64 @@
+import axios from "axios";
+import {  useState } from "react";
 
 const Add = () => {
+  const bookData = {
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  };
+  const [book, setBook] = useState(bookData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios("https://localhost:8800/books", book);
+      console.log(res.data);
+      setBook(bookData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBook((prevBook) => ({
+      ...prevBook,
+      [name]: value,
+    }));
+  };
   return (
     <div>
-        <form>
-            <label>ADD new Book</label>
-           <input type="text" placeholder="title" value={''} /> 
-           <input type="text" placeholder="desc" value={''} /> 
-           <input type="number" placeholder="price" value={''} /> 
-           <input type="text" placeholder="cover" value={''} /> 
-        </form>
+      <form onSubmit={handleSubmit}>
+        <label>ADD new Book</label>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="title"
+          value={book.title}
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="desc"
+          value={book.desc}
+        />
+        <input
+          onChange={handleChange}
+          type="number"
+          placeholder="price"
+          value={book.price}
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="cover"
+          value={book.cover}
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Add
+export default Add;
